3c32fe9c0fad70c81369b55aabb4f4f68075dec2
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-10 14:28:00 -0300

    Atualização.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-    // botão da barra lateral
+
+// botão da barra lateral
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('open');
-  }
-  // ano do rodapé
-  const yearElement = document.getElementById('year');
-  const currentYear = new Date().getFullYear();
-  yearElement.textContent = currentYear;  
+}
+// ano do rodapé
+const yearElement = document.getElementById('year');
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;  
+
+/*const checkButton = () => {
+    const verify = document.getElementById('submit-button');
+    if (!verify) {
+        return console.log('Não foi encontrado')
+    }
+    verify.addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log('Twice', location.href)
+    })
+    return console.log('Foi encontrado', verify)
+} 
+checkButton();*/ 
