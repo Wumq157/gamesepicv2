b573e4f62a00c82fa06530e4dffeb6f2ca186bee
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-10 14:29:05 -0300

    Update

--- js/html.js
+++ js/html.js
@@ -1,50 +1,51 @@
 function adicionarConteudoPrincipal(conteudo) {
-  var novoHTML = `<!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="style/reset.css">
-        <link rel="stylesheet" type="text/css" href="style/style.css">
-        <link rel="stylesheet" type="text/css" href="style/barra.css">
-        <title>Document</title>
-    </head>
-    <body>
-        <header>
-            <h1>Cabeçalho</h1>
-        </header>
-        <div class="container">
-            <aside class="sidebar" id="sidebar">
-            <label class="switch">
-            <input type="checkbox" onclick="toggleSidebar()">
-            <span class="slider"></span>
-          </label>          
-                <div class="btn-container">
-                <ul class="menu">
-                    <li><a class="btn" href="index.html">Home</a></li>
-                    <li><a class="btn" href="clips.html">Clips</a></li>
-                    <li><a class="btn" href="contato.html">Contato</a></li>
-                    <li><a class="btn" href="loja.html">Loja</a></li>
-                    <li><a class="btn" href="imc.html">Saúde</a></li>
-                    <li><a class="btn" href="jogos.html">Jogos</a></li>
-                    <li><a class="btn" href="luan.html">Academia</a></li>
-                    <li><a class="btn" href="suporte.html">Suporte</a></li>
-                    <li><a class="btn" href="https://wumq157.github.io/GamesEpic/">GamesEpic</a></li>
-                    <!-- Adicione mais opções conforme necessário -->
-                </ul>
-                </div>
-            </aside>
-            <main>${conteudo}</main>
-        </div>
-        <footer>
-            <div class="footer-content">
-                <p>&copy;<span id="year"></span>GameEpicV2</p>
-            </div>
-        </footer>
-        <script src="js/script.js"></script>
-    </body>
-    </html>`;
-
-  document.documentElement.insertAdjacentHTML('beforeend', novoHTML);
-}
+    var novoHTML = `<!DOCTYPE html>
+      <html lang="pt-br">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" type="text/css" href="style/reset.css">
+          <link rel="stylesheet" type="text/css" href="style/style.css">
+          <link rel="stylesheet" type="text/css" href="style/barra.css">
+          <title>Document</title>
+      </head>
+      <body>
+          <header>
+              <h1>Cabeçalho</h1>
+          </header>
+          <div class="container">
+              <aside class="sidebar" id="sidebar">
+              <label class="switch">
+              <input type="checkbox" onclick="toggleSidebar()">
+              <span class="slider"></span>
+            </label>          
+                  <div class="btn-container">
+                  <ul class="menu">
+                      <li><a class="btn" href="index.html">Home</a></li>
+                      <li><a class="btn" href="clips.html">Clips</a></li>
+                      <li><a class="btn" href="contato.html">Contato</a></li>
+                      <li><a class="btn" href="loja.html">Loja</a></li>
+                      <li><a class="btn" href="imc.html">Saúde</a></li>
+                      <li><a class="btn" href="jogos.html">Jogos</a></li>
+                      <li><a class="btn" href="luan.html">Academia</a></li>
+                      <li><a class="btn" href="suporte.html">Suporte</a></li>
+                      <li><a class="btn" href="https://wumq157.github.io/GamesEpic/">GamesEpic</a></li>
+                      <!-- Adicione mais opções conforme necessário -->
+                  </ul>
+                  </div>
+              </aside>
+              <main>${conteudo}</main>
+          </div>
+          <footer>
+              <div class="footer-content">
+                  <p>&copy;<span id="year"></span>GameEpicV2</p>
+              </div>
+          </footer>
+          <script src="js/script.js"></script>
+      </body>
+      </html>`;
+  
+    document.documentElement.insertAdjacentHTML('beforeend', novoHTML);
+  }
+  
--- js/imc.js
+++ js/imc.js
@@ -1,7 +1,8 @@
+window.alert("Olá")
+var peso = Number(window.prompt("Diga qual o seu peso:"));
+var altura = Number(window.prompt("Diga qual a sua altura:"));
+var altura2 = altura ** 2;
+var imc = peso / altura2;
+var imcFormatted = parseFloat(imc).toFixed(2);
 
-    var peso = Number(window.prompt("Diga qual o seu peso:"));
-    var altura = Number(window.prompt("Diga qual a sua altura:"));
-    var altura2 = altura ** 2;
-    var imc = peso / altura2;
-    var imc = parseFloat(imc).toFixed(2);
-    document.write(`Seu IMC é de ${imc}, com base nesse valor você deve procurar a ajuda correta.`);
+document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, com base nesse valor você deve procurar a ajuda correta.`;
