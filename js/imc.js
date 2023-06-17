1aeb42bb7ec6ef98ee7671735cd22b271208ba72
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-16 21:36:51 -0300

    Add files via upload

--- js/imc.js
+++ js/imc.js
@@ -1,3 +1,4 @@
+
 function imcCalcule() {
     var peso = Number(document.getElementById('peso').value);
     peso = Math.abs(peso); // Converter valor negativo para positivo
@@ -13,11 +14,23 @@ function imcCalcule() {
     var altura2 = altura ** 2;
     var imc = peso / altura2;
     var imcFormatted = imc.toFixed(2);
+if (imcFormatted <= 18.4) {
+    document.getElementById("imcValue").innerHTML = `Seu IMC é de ${imcFormatted}, Você está na linha de magreza, recomendamos que procure ajuda de <strong>um</strong> nutricionista.`;
+} else if (imcFormatted <= 24.9) {
+    document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, Você está com um pesso saúdavel com base no seu imc.`;
+} else if (imcFormatted <= 29.9){
+    document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, Você está sobre peso de acordo com o seu imc, recomendamos que procure ajuda de um nutricionista.`;
+} else if (imcFormatted <= 34.9){
+    document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, Você está com no primeiro grau de obesidade de acordo com seu imc, recomendamos que procure ajuda de um nutricionista.`;
+}  else if (imcFormatted <= 39.9){
+    document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, Você está no segundo grau de obesidade de acordo com seu imc, recomendamos que procure ajuda de um nutricionista.`;
+}  else if (imcFormatted > 40){
+    document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, Você está no terceiro grau de obesidade de acordo com seu imc, recomendamos que procure ajuda de um nutricionista URGENTE!!!`;
+} 
 
-    document.getElementById("imcValue").textContent = `Seu IMC é de ${imcFormatted}, com base nesse valor você deve procurar a ajuda correta.`;
-    alturaInput.value = alturaFormatted; // Atualizar o campo de altura com o valor formatado
+
+alturaInput.value = alturaFormatted; // Atualizar o campo de altura com o valor formatado
 }
-
 document.getElementById("submit-button").addEventListener("click", function(event) {
     event.preventDefault();
     imcCalcule();
