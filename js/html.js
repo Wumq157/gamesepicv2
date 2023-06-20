8f439b7242f7db2b6d74145a508b3ee321191978
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-20 11:33:16 -0300

    .

--- js/html.js
+++ js/html.js
@@ -5,10 +5,10 @@ function adicionarConteudoPrincipal(conteudo) {
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="shortcut icon" href="https://i.imgur.com/7KwrsKR.png">
           <link rel="stylesheet" type="text/css" href="style/reset.css">
           <link rel="stylesheet" type="text/css" href="style/style.css">
           <link rel="stylesheet" type="text/css" href="style/barra.css">
-          <link rel="shortcut icon" href="https://i.imgur.com/7KwrsKR.png">
       </head>
       <body>
           <header>
