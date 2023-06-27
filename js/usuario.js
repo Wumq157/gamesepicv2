00f94b317ba3f01a4f78239c1513cea5bb56ec81
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-06-27 12:52:26 -0300

    Correções

--- js/usuario.js
+++ js/usuario.js
@@ -1,15 +1,8 @@
-setTimeout(function() {
-  adicionarConteudoPrincipal();
-}, 2);
+setTimeout(adicionarNome, 2);
 
-function adicionarConteudoPrincipal() {
-  const nomeElement = document.querySelector('.nome');
+function adicionarImagemPerfil() {
   const perfilElement = document.querySelector('#perfil');
-
-  const nome = 'Wumq';
   const profileImageUrl = 'https://static-cdn.jtvnw.net/jtv_user_pictures/14f847f0-a15a-4df8-8dc7-9e32795f589d-profile_image-300x300.jpeg';
-
-  nomeElement.textContent = nome;
 
   if (profileImageUrl) {
     const imgElement = document.createElement('img');
@@ -17,4 +10,14 @@ function adicionarConteudoPrincipal() {
     perfilElement.innerHTML = '';
     perfilElement.appendChild(imgElement);
   }
+}
+
+function adicionarNome() {
+  const nomeElement = document.querySelector('.nome');
+  
+  const nome = 'Wumq';
+  
+  nomeElement.textContent = nome;
+  
+  adicionarImagemPerfil();
 }
